9c0d215677238d55d8851a94d7a63b04a0a5a4ed
Rakel Macedo <macedorakel35@gmail.com>
2023-10-20 02:41:21 -0300

    Finalizando TCP e Ethernet

--- scanners.py
+++ scanners.py
@@ -1,12 +1,7 @@
 import socket
 import struct
-
-
-def PING():
-    pass
-
-def UDP():
-    pass
+import time
+from mongodb import Insert_MongoDB
 
 
 def Ethernet():
@@ -19,8 +14,10 @@ def Ethernet():
     }
 
     try:
+        start_time = time.time()  # Marca o tempo de início
 
-        while True:
+        while time.time() - start_time < 10:  # Roda por 10 segundos
+
             raw_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
             # Capture um pacote Ethernet
@@ -32,7 +29,6 @@ def Ethernet():
 
             eth_dest_mac, eth_src_mac, eth_type = struct.unpack("!6s6sH", eth_header)
 
-            header_ethernet = "\nCabeçalho Ethernet:"
             mac_destino = ':'.join('%02x' % b for b in eth_dest_mac)
             mac_origem = ':'.join('%02x' % b for b in eth_src_mac)
 
@@ -40,22 +36,21 @@ def Ethernet():
                 eth_type_str = ethertype_dict[eth_type]
             else:
                 eth_type_str = hex(eth_type)
-                print(f"Desconhecido: {eth_type_str}")
             
             tipo = eth_type_str
 
-            payload_hexadecimal = "\nPayload:"
             eth_payload_str = ':'.join(f'{b:02x}' for b in eth_payload)
 
-            separador = "\n" + "-" * 112
+            ethernet_dict = {
+                "MAC de destino": mac_destino,
+                "MAC de origem": mac_origem,
+                "Tipo": tipo,
+                "Payload Hexadecimal": eth_payload_str,
+            }
 
-            print(header_ethernet)
-            print(f"MAC de destino: {mac_destino}")
-            print(f"MAC de origem: {mac_origem}")
-            print(f"Tipo: {tipo}")
-            print(payload_hexadecimal)
-            print(eth_payload_str)
-            print(separador)
+        Insert_MongoDB(ethernet_dict, 'ETHERNET')
+
+        print("Captura de pacotes encerrada após 10 segundos.")
 
     except KeyboardInterrupt:
         print("Captura de pacotes encerrada.")
@@ -63,14 +58,15 @@ def Ethernet():
     finally:
         raw_socket.close()
 
+    return ethernet_dict
+
 
 def TCP():
-    
     try:
         raw_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+        start_time = time.time()  # Marca o tempo de início
 
-        while True:
-            
+        while time.time() - start_time < 5:  # Roda por 10 segundos
             # Capture um pacote Ethernet
             packet, addr = raw_socket.recvfrom(65535)
 
@@ -81,7 +77,6 @@ def TCP():
             eth_dest_mac, eth_src_mac, eth_type = struct.unpack("!6s6sH", eth_header)
 
             if eth_type == 0x0800:  # Se for um pacote IPv4
-                
                 # Analise o payload IP e verifique se é um pacote TCP
                 ip_header = eth_payload[:20]
                 ip_version_ihl, ip_tos, ip_total_length, ip_id, ip_flags_frag_offset, ip_ttl, ip_protocol, ip_checksum, ip_src, ip_dst = struct.unpack("!BBHHHBBH4s4s", ip_header)
@@ -89,26 +84,34 @@ def TCP():
                 ihl = (ip_version_ihl & 0xF) * 4  # O tamanho do cabeçalho IP (IHL) é o valor mais baixo de 4 bits
 
                 if ip_protocol == 6:  # Se for um pacote TCP
-                    tcp_header = eth_payload[ihl:ihl+20]  # O cabeçalho TCP segue o cabeçalho IP
+                    tcp_header = eth_payload[ihl:ihl + 20]  # O cabeçalho TCP segue o cabeçalho IP
                     tcp_src_port, tcp_dst_port, tcp_seq, tcp_ack, tcp_offset_reserved, tcp_flags, tcp_window, tcp_checksum, tcp_urg_ptr = struct.unpack("!HHLLBBHHH", tcp_header)
 
-                    print("\nCabeçalho TCP:")
-                    print(f"Porta de origem: {tcp_src_port}")
-                    print(f"Porta de destino: {tcp_dst_port}")
-                    print(f"Número de sequência: {tcp_seq}")
-                    print(f"Número de reconhecimento: {tcp_ack}")
-                    print(f"Flags: {hex(tcp_flags)}")
-                    print(f"Tamanho da janela: {tcp_window}")
-                    print(f"Checksum: {hex(tcp_checksum)}")
-                    print(f"Ponteiro urgente: {tcp_urg_ptr}")
+                    # Crie um dicionário com as informações do cabeçalho TCP
+                    tcp_dict = {
+                        "Porta de origem": tcp_src_port,
+                        "Porta de destino": tcp_dst_port,
+                        "Numero de sequencia": tcp_seq,
+                        "Numero de reconhecimento": tcp_ack,
+                        "Flags": hex(tcp_flags),
+                        "Tamanho da janela": tcp_window,
+                        "Checksum": hex(tcp_checksum),
+                        "Ponteiro urgente": tcp_urg_ptr
+                    }
 
-                    print("\n" + "-" * 112)
-
+                    Insert_MongoDB(tcp_dict, 'TCP')
+                    
+        print("Captura de pacotes encerrada após 10 segundos.")
+        
     except KeyboardInterrupt:
-        print("Captura de pacotes encerrada.")
-
+        print("Captura de pacotes encerrada manualmente.")
+    
+    except Exception as e:
+        print("Erro durante a captura de pacotes:", str(e))
+    
     finally:
         raw_socket.close()
 
+    return tcp_dict
 
-TCP()
+
