4f823dcf4c4256c0adb4a7a52e35d3c1f1c69830
Rakel Macedo <macedorakel35@gmail.com>
2023-10-20 02:38:25 -0300

    Add Insercao no MongoDB

--- mongodb.py
+++ mongodb.py
@@ -1,3 +1,28 @@
-# Funções que faz a conexão com o MongoDB e envia os dados para lá
+from pymongo import MongoClient
 
+
+# Função que insere os dados no MongoDB
+def Insert_MongoDB(data, collection):
+
+    try:
+        # Tenta criar uma conexão com o servidor MongoDB local
+        client = MongoClient("mongodb://localhost:27017/")
+
+        # Acessa o banco de dados "port_scans"
+        db = client["CoreNetwork"]
+
+        # Acessa a coleção "scan_results" dentro do banco de dados
+        collection = db[collection]
+
+        # Insere os dados no MongoDB
+        collection.insert_one(data)
+
+        # Fecha a conexão com o MongoDB
+        client.close()
+
+        print("\n[+] Resultados do scan inseridos no MongoDB com sucesso!\n")
+
+    except Exception as e:
+        print(f"\n[-] Ocorreu um erro ao inserir os resultados no MongoDB, veja abaixo:\n{e}")
+        print("\n[-] Certifique-se de que você possui o MongoDB instalado e em execução.\n")
 
